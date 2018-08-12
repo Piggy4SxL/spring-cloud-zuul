b6c2fa411c54732303b7f38d072b18618498f454
Tulip_SxL <tulip_sxl@outlook.com>
2018-08-12 09:17:36 +0800

    [INIT]

---
+++ src/main/java/com/oc/sxl/zuul/filter/PostFilter.java
@@ -0,0 +1,44 @@
+package com.oc.sxl.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.UUID;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
+
+/**
+ * @author SxL
+ * Created on 7/27/2018 11:45.
+ */
+@Component
+public class PostFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletResponse response = requestContext.getResponse();
+
+        response.setHeader("X-Foo", UUID.randomUUID().toString());
+        return null;
+    }
+}
